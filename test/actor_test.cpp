91edaced7d70ef4bcb3b44eebcc799864032fa7f
Szymon Nowik <heftyy@gmail.com>
2015-10-06 15:17:51 +0200

    fixed linking errors updated linux to boost 1.59

--- test/actor_test.cpp
+++ test/actor_test.cpp
@@ -11,42 +11,6 @@
 
 BOOST_AUTO_TEST_SUITE( actor_suite )
 
-    BOOST_AUTO_TEST_CASE(ActorSTUPIDTEST) {
-        thread_pool tp(2);
-        /*
-        int task_counter = 0;
-
-        tp.push([&task_counter](int x) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            BOOST_LOG_TRIVIAL(debug) << "pck task " << x;
-            task_counter++;
-        }, 1);
-
-        tp.push([&task_counter](int x) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(600));
-            BOOST_LOG_TRIVIAL(debug) << "pck task " << x;
-            task_counter++;
-        }, 2);
-
-        tp.push([&task_counter](int x) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(300));
-            BOOST_LOG_TRIVIAL(debug) << "pck task " << x;
-            task_counter++;
-        }, 3);
-
-        tp.push([&task_counter](int x) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            BOOST_LOG_TRIVIAL(debug) << "pck task " << x;
-            task_counter++;
-        }, 4);
-
-        //wait for all tasks to finish
-        tp.stop(true);
-
-        BOOST_CHECK_EQUAL(task_counter, 4);
-        */
-    }
-
     BOOST_AUTO_TEST_CASE(ActorRefTest) {
         actor_ref test_ref = actor_ref("test_actor", "test_system");
 
